31d5b23b2eb31d5bbc9c95b6e44cd9c70ccc9d3e
Ronikhudanyan <Ronikhudanyan@gmail.com>
2021-01-05 21:08:31 -0800

    commit message

---
+++ src/pages/about/about.component.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./about.styles.css";
+import Profile from "../../assets/img/profile/self.png";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+
+const About = () => {
+  return (
+    <div id="about">
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mb-2 mr-2 ">
+                <Image className="profile justify-content-end" alt="profile" src={Profile} thumbnail fluid />
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className=" align-items-start p-2 my-details rounded">
+                Hi there! I am <strong>&nbsp;Roni Khudanyan</strong>
+                <br />A passionate programmer, born and brought up in Los Angeles With Armenian roots and tech in my DNA.
+                <br />
+                I come from a long line of devs which have taught me tech from the moment I could speak, this in turn gave me a passion that cannot be taught.
+                <br />
+                My approach blends the passion I was brought up with and the 6 years of experience in the field.
+                <br />
+                My success comes from my ability to step back, take a look at the bigger picture and apply skills and experience. 
+                <br />
+                I am able to pick apart an issue to apply logic and critical thinking, which then delivers a product that is beyond expectations.
+                <br/> 
+                These traits combined directly contribute to the pride I hold with my work ethic.
+                <br />I can get the job done in ways that will exceed expectations and set presidents for the future.
+                <br /> <br />
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        Let's talk
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://docs.google.com/document/d/1AXpxcRHHSDM8XNKAtTMxsiVcLnZSzf29dFu-oxSp2K0/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-success">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://github.com/Ronikhudanyan" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://www.linkedin.com/in/ronikay818/" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-info">
+                        LinkedIn
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default About;
